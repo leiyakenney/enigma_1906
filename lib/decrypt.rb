bc183117249d20bbf037ccf52bc1d8b06fdc3c7a
Leiya Kenney <leiya.kenney@gmail.com>
2019-07-29 15:00:00 -0600

    Updates decrypt to actually work properly

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -7,11 +7,11 @@ require './modules/enigma_helper_module'
 
 enigma = Enigma.new
 
-message_for_decryption = File.open('ciphertext.txt', 'r')
+message_for_decryption = File.open('encrypted.txt', 'r')
 
 decrypted_msg = File.open('decrypted.txt', 'w')
 
-decrypted_text = enigma.decrypt(message_for_decryption.read)
+decrypted_text = enigma.decrypt(message_for_decryption.read, ARGV[2], ARGV[3])
 decrypted_msg.write(decrypted_text[:decryption])
 decrypted_msg.close
 
