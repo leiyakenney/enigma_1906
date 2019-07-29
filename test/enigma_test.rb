76f3dea82ed6822088b4996221dc0c1408ad2086
Leiya Kenney <leiya.kenney@gmail.com>
2019-07-29 15:00:00 -0600

    Puts enigma tests back in after I accidentally deleted them

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -14,7 +14,46 @@ class EnigmaTest < Minitest::Test
   def test_character_set
     expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
     assert_equal expected, @enigma.char_set
-  end  
+  end
+
+  def test_special_chars
+    expected = {
+      ':' => 6,
+      ')' => 7,
+      '!' => 13
+    }
+    assert_equal expected, @enigma.special_chars("hello :)world!")
+  end
+
+  def test_hash_for_shifting
+    expected = {
+      "A" => ['h', 'o', 'r'],
+      "B" => ['e', ' ', 'l'],
+      "C" => ['l', 'w', 'd'],
+      "D" => ['l', 'o']
+    }
+    assert_equal expected, @enigma.hash_for_shifting("hello world", '12345', '280719')
+    assert_equal expected, @enigma.hash_for_shifting("hello :)world!", '12345', '280719')
+  end
+
+  def test_shift_hash
+    expected = {
+      "A" => ['z', 'f', 'i'],
+      "B" => ['j', 'e', 'q'],
+      "C" => ['y', 'i', 'q'],
+      "D" => ['d', 'g']
+    }
+
+    assert_equal expected, @enigma.shift_hash("hello world", '12345', '280719')
+  end
+
+  def test_shifted_hash_to_msg
+    assert_equal "zjydfeigiqq", @enigma.shifted_hash_to_msg("hello world", '12345', '280719')
+  end
+
+  def test_total_msg
+    assert_equal "zjydfe:)igiqq!", @enigma.total_msg("hello :)world!", '12345', '280719')
+  end
 
   def test_it_encrypts
     expected = {
@@ -23,7 +62,6 @@ class EnigmaTest < Minitest::Test
       date: "040895"
     }
     assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
-    assert_equal "02715", @enigma.encrypt("hello world", "02715", "040895")[:key]
   end
 
   def test_it_decrypts
