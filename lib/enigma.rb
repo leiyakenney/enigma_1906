cf98b570182927b7abfcd06927795a957aaba298
Leiya Kenney <leiya.kenney@gmail.com>
2019-07-29 11:29:05 -0600

    Creates hash_for_shift method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,12 +1,13 @@
 class Enigma
-  attr_reader :message, :key, :offset, :shift, :msg_arr
+  attr_reader :char_set
 
-  def initialize(message, key, date)
-    @message = message
-    @key = key
-    @offset = date
-    @shift = shift
-    @msg_arr = @message.split('')
+  def initialize
+    # @message = message
+    # @key = key
+    # @offset = offset
+    # @shift = shift
+    @char_set = ("a".."z").to_a << " "
+    # @msg_arr = @message.split('')
   end
 
   # def array_for_shift
@@ -17,17 +18,33 @@ class Enigma
   #   letter_arrays
   # end
 
-  def array_for_shift
-    a_array = []
-    b_array = []
-    c_array = []
-    d_array = []
-    @mst_arr.map.with_index do |ltr|
-      if % 4 == 0
-        a_array << ltr
-      elsif % 4
-
+  def hash_for_shift(message)
+    shift_hash = {"A" => [], "B" => [], "C" => [], "D" => []}
+    index = 0
+    message.split('').map do |letter|
+      if @char_set.include?(letter)
+        if index == 0
+          shift_hash["A"] << letter
+        elsif index != 0
+          if index % 4 == 0
+            shift_hash["A"] << letter
+          elsif index % 4 == 1
+            shift_hash["B"] << letter
+          elsif index % 4 == 2
+            shift_hash["C"] << letter
+          elsif index % 4 == 3
+            shift_hash["D"] << letter
+          end
+        end
+        index += 1
+      end
+    end
+    shift_hash
   end
+
+  # def shift_hash
+  #
+  # end
   # def encrypt(message, key, date)
   #
   # end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -3,25 +3,37 @@ require './test/test_helper'
 class EnigmaTest < Minitest::Test
 
   def setup
-    @enigma = Enigma.new('hello world', 12345, 280719)
+    @enigma = Enigma.new
   end
 
   def test_it_exists
     assert_instance_of Enigma, @enigma
   end
 
-  def test_msg_to_array
-    assert_equal ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'], @enigma.msg_arr
+  def test_character_set
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal expected, @enigma.char_set
   end
 
-  def test_array_for_shift
+  # def test_msg_to_array
+  #   assert_equal ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'], @enigma.msg_arr
+  # end
+
+  def test_hash_for_shift
     expected = {
       "A" => ['h', 'o', 'r'],
       "B" => ['e', ' ', 'l'],
       "C" => ['l', 'w', 'd'],
       "D" => ['l', 'o']
     }
-    assert_equal expected, @enigma.array_for_shift
+    assert_equal expected, @enigma.hash_for_shift("hello world")
+    assert_equal expected, @enigma.hash_for_shift("hello :)world!")
+  end
+
+  def test_shift_hash
+    expected = {
+      "A" => []
+    }
   end
 
   # def test_it_encrypts
