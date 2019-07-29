d299b88ed3c77a1fd3b0f2c7b7dbb68dc900f7b6
Leiya Kenney <leiya.kenney@gmail.com>
2019-07-29 06:32:39 -0600

    Updates key to take in a value from the start vs. being random

--- lib/key.rb
+++ lib/key.rb
@@ -1,25 +1,25 @@
 class Key
-  attr_reader :key
+  attr_reader :key_val
 
-  def initialize
-    @key = rand(1..99_999).to_s.rjust(5, '0')
+  def initialize(key_val)
+    @key_val = key_val.to_s.rjust(5, '0')
   end
 
-  # def random_key
-  #   rand(1..99_999).to_s.rjust(5, '0')
-  # end
+  def random_key
+    rand(1..99_999).to_s.rjust(5, '0')
+  end
 
   # def given_key
   #   @key = given_key
   # end
 
   def keys_hash
-    key_array = key.split('') #|| key_array = given_key.split('')
+    key_array = key_val.split('')
     hash_of_keys = Hash.new(0)
-      hash_of_keys["A"] += (key_array[0] + key_array[1]).to_i
-      hash_of_keys["B"] += (key_array[1] + key_array[2]).to_i
-      hash_of_keys["C"] += (key_array[2] + key_array[3]).to_i
-      hash_of_keys["D"] += (key_array[3] + key_array[4]).to_i
+      hash_of_keys["A"] = (key_array[0].to_i + key_array[1].to_i)
+      hash_of_keys["B"] = (key_array[1].to_i + key_array[2].to_i)
+      hash_of_keys["C"] = (key_array[2].to_i + key_array[3].to_i)
+      hash_of_keys["D"] = (key_array[3].to_i + key_array[4].to_i)
     hash_of_keys
   end
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -4,25 +4,33 @@ require './test/test_helper'
 class KeyTest < Minitest::Test
 
   def setup
-    @key = Key.new
+    @key = Key.new(12345)
+    @key_2 = Key.new(678)
   end
 
   def test_it_exists
     assert_instance_of Key, @key
+    assert_instance_of Key, @key_2
   end
 
   def test_key_attributes
-    assert @key.key.between?('00001', '99999')
-    assert_equal 5, @key.key.length
+    assert @key.key_val.between?('00001', '99999')
+    assert @key_2.key_val.between?('00001', '99999')
+    assert_equal 5, @key.key_val.length
+    assert_equal 5, @key_2.key_val.length
+  end
+
+  def test_random_key_attributes
+    assert @key.random_key.between?('00001', '99999')
+    assert_equal 5, @key.random_key.length
   end
 
   def test_key
-    @key = stub(key: '12345')
-    assert_equal '12345', @key.key
+    assert_equal '12345', @key.key_val
+    assert_equal '00678', @key_2.key_val
   end
 
   def test_keys_hash
-    @key = stub(keys_hash: ({"A" => 3, "B" => 5, "C" => 7, "D" => 9}))
     expected = {
       "A" => 3,
       "B" => 5,
@@ -33,8 +41,5 @@ class KeyTest < Minitest::Test
     assert_equal expected, @key.keys_hash
   end
 
-  # def test_given_key_attributes
-  #   assert @key.given_key.between?('00001', '99999')
-  # end
 
 end
